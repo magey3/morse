13654057ebe1c2607c7585f7b9c852ac02285119
magey3 <magey3333@gmail.com>
2021-01-14 12:48:33 +0100

    Added argument parsing and file input/output

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use structopt::StructOpt;
 
 #[cfg(test)]
 mod tests {
@@ -37,9 +38,33 @@ fn decode_morse(code: String, hash_map: HashMap<String, char>) -> String {
 	out
 }
 
-fn main() {
-	let decoder: HashMap<String, char> = create_decode_map("morse.txt");
-	println!("{}", decode_morse(String::from("... --- .../... --- ..."), decoder));
+#[derive(StructOpt)]
+struct Cli {
+	#[structopt(parse(from_os_str), short = "i", long = "input_file")]
+	path: Option<std::path::PathBuf>,
 
-    println!("Hello, world!");
+	#[structopt(parse(from_os_str), short = "o", long = "output_file")]
+	output_path: Option<std::path::PathBuf>,
+}
+
+fn read_from_stdin() -> String{
+	let mut stdin = String::new();
+	use std::io::BufRead;
+	std::io::stdin().lock().read_line(&mut stdin).unwrap();
+	stdin
+}
+
+fn main() {
+	let args = Cli::from_args();
+	let decoder: HashMap<String, char> = create_decode_map("morse.txt");
+	
+	let out = match args.path {
+		Some(path) => decode_morse(std::fs::read_to_string(path).expect("Could not read input file"), decoder),
+		None => decode_morse(read_from_stdin(), decoder),
+	};
+	
+	match args.output_path {
+		Some(path) => std::fs::write(path, out).expect("Could not write to output file"),
+		None => println!("{}", out)
+	}
 }
